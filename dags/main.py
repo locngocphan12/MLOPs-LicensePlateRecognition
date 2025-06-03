f3b91ee4a9039b7753655f4e7e9b9d26fa88e13b
Twind <locngocphan12@gmail.com>
2025-06-03 18:26:30 +0700

    test ver1

--- dags/main.py
+++ dags/main.py
@@ -151,7 +151,9 @@ with DAG(
 
         metadata = log_dataset_metadata("src/data.yaml")
         final_path = os.path.join("runs/detect", "best_model.pt")
-        shutil.copy(result["best_model"], final_path)
+        shared_path = "/app/shared/best_model.pt"
+        # changed
+        shutil.copy(result["best_model"], shared_path)
 
         mlflow.set_tracking_uri(MLFLOW_URI)
         mlflow.set_experiment("YOLOv8n_Finetune")
